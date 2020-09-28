980da4ad2b3f6a10f0636c70302569eb83084aa0
Leriatod <artem.viznuk@gmail.com>
2020-09-28 15:58:47 +0300

    Seeding Feature types for Phone

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -6,6 +6,7 @@ namespace PhoneExp.Data
     public class AppDbContext : DbContext
     {
         public DbSet<Phone> Phones { get; set; }
+        public DbSet<FeatureType> FeatureTypes { get; set; }
         public AppDbContext(DbContextOptions<AppDbContext> options) 
             : base(options)
         {       
---
+++ Migrations/20200928124138_SeedingFeatureTypes.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PhoneExp.Migrations
+{
+    public partial class SeedingFeatureTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("SET IDENTITY_INSERT FeatureTypes ON");
+            migrationBuilder.Sql("INSERT INTO FeatureTypes(Id, Name) VALUES (1, 'Price')");
+            migrationBuilder.Sql("INSERT INTO FeatureTypes(Id, Name) VALUES (2, 'Operating System')");
+            migrationBuilder.Sql("INSERT INTO FeatureTypes(Id, Name) VALUES (3, 'Screen Size')");
+            migrationBuilder.Sql("INSERT INTO FeatureTypes(Id, Name) VALUES (4, 'Display Quality')");
+            migrationBuilder.Sql("INSERT INTO FeatureTypes(Id, Name) VALUES (5, 'Design')");
+            migrationBuilder.Sql("INSERT INTO FeatureTypes(Id, Name) VALUES (6, 'Camera')");
+            migrationBuilder.Sql("INSERT INTO FeatureTypes(Id, Name) VALUES (7, 'Processor')");
+            migrationBuilder.Sql("INSERT INTO FeatureTypes(Id, Name) VALUES (8, 'Ram')");
+            migrationBuilder.Sql("INSERT INTO FeatureTypes(Id, Name) VALUES (9, 'Internal Storage')");
+            migrationBuilder.Sql("INSERT INTO FeatureTypes(Id, Name) VALUES (10, 'Battery Life')");
+            migrationBuilder.Sql("SET IDENTITY_INSERT FeatureTypes OFF");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM FeatureTypes");
+        }
+    }
+}
